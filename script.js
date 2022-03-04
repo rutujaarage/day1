6341f4bf7006fce8930408d10e465739203e20ca
rutujaarage <rutujaarage99@gmail.com>
2022-03-04 17:25:53 +0530

    loops

---
+++ script.js
@@ -0,0 +1,77 @@
+
+//three or more than three condition then use switch case.
+
+// let gender ="a";
+// switch(gender){
+//     case "m":
+//         console.log("male");
+//         break;
+//     case "f":
+//         console.log("female");
+//         break;
+//     default:
+//             console.log("transgender");
+//             break;
+
+// }
+
+//take age as variable age<18 = child ,, 18 <age<40 adult ,, age>40 = old age...
+
+// let age=25;
+// switch(true){
+//     case age<18:
+//         console.log("this is child") ;
+//         break;
+//     case 18<age<40:
+//         console.log(" this man adult");
+//         break;
+//     default:
+//         console.log(" the man was dead");
+//         break;    
+// }
+
+
+//take variable name as day , it's posible values are 0 1 2 3 4 5 6 ....
+
+// let day=5;
+// switch(day){
+//     case 0:
+//         console.log("monday") ;
+//         break;
+//     case 1:
+//         console.log(" tuesday");
+//         break;
+//     case 2:
+//         console.log(" wensday");
+//         break;
+//     case 3:
+//         console.log("thursday");
+//         break;
+//     case 4:
+//         console.log(" friday");
+//         break;
+//     case 5:
+//         console.log(" saturday");
+//         break;
+        
+//     default:
+//         console.log(" sunday");
+//         break;    
+// }
+
+
+// .............................iteration loops...........................
+
+//var a;// it just declare..
+//var a=8; // we are initialization the variable with the value of 8.
+
+
+//.......for loop.....
+// for(let i=1;i<10;i++){
+//     console.log(i);
+// }
+
+for(let i=1;i<=10;i++){
+     console.log("19"+ "*" + i + "=" + "19"*i);
+     }
+
